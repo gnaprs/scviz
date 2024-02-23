ea69dba97f5f6713448050283853af84f53581a0
Marion Pang <sr_pang@hotmail.com>
2024-02-23 14:28:39 -0800

    upload package to testpypi for testing

--- README.md
+++ README.md
@@ -2,5 +2,12 @@
 scviz is a Python package for scientific visualization of single cell proteomics (and more soon!) data.
 
 ## Installation
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install scviz. Currently, scviz is only available on testPyPi:
+`python -m pip install -i https://test.pypi.org/simple/ scviz==0.0.2`
 
-Use the package manager [pip](https://pip.pypa.io/en/stable/) to install scviz.
+
+## To-Do List
+1. Add more visualization features for volcano plots.
+2. Extend support for GSEA and GO analysis
+3. Add more comprehensive documentation.
+4. Implement unit tests for better code reliability.
--- src/scviz/setup.py
+++ src/scviz/setup.py
@@ -1,39 +1,3 @@
-# scviz is a package for visualizing single cell data
-# Author: Marion Pang
-# Created: 2024-02-07
-# version: 0.1
-
-from setuptools import setup, find_packages
-
-VERSION = '0.1'
-DESCRIPTION = 'A package for visualizing single cell data'
-LONG_DESCRIPTION = 'A package for visualizing single cell data'
-
-setup(
-       # the name must match the folder name 'verysimplemodule'
-        name="scviz", 
-        version=VERSION,
-        author="Marion Pang",
-        author_email="<sr_pang@hotmail.com>",
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
-        packages=find_packages(),
-        install_requires=['pandas','numpy','seaborn','upsetplot'],
-        keywords=['single-cell', 'proteomics'],
-        classifiers= [
-            "Development Status :: 3 - Alpha",
-            "Programming Language :: Python :: 3",
-            "Operating System :: MacOS :: MacOS X",
-            "Operating System :: Microsoft :: Windows",
-        ]
-)
-
-# TO DO:
-# - add visualization of lcms data (?)
-# - 3D PCA plot
-# - volcano plot for differential expression
-# - GO term enrichment and GSEA
-
 import pandas as pd
 import numpy as np
 import re
