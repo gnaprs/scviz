a5126078cd1050055681d65bacdf23dc3d21b746
Marion Pang <sr_pang@hotmail.com>
2025-02-05 17:03:46 -0800

    trying to fix dependencies

--- src/scviz/setup.py
+++ src/scviz/setup.py
@@ -18,3 +18,25 @@ mouse_file_path = os.path.join(current_dir, '__assets__', 'MostStable_Mouse.csv'
 # Read the CSV files
 housekeeping_HUMAN = pd.read_csv(human_file_path)
 housekeeping_MOUSE = pd.read_csv(mouse_file_path)
+
+# set function to get date and time
+import datetime
+def get_datetime():
+    now = datetime.datetime.now()
+    return now.strftime("%Y-%m-%d %H:%M:%S")
+
+# print versions of all dependencies of scviz
+def print_versions():
+    dependencies = ["numpy","pandas","matplotlib","seaborn",
+                    "upsetplot","sklearn","scipy","umap",
+                    "adjustText","anndata","requests","matplotlib_venn","pyarrow"]
+
+    print("scViz version: 0.1.0")
+    print("Date and time: ", get_datetime())
+    print("Dependencies:")
+    for package in dependencies:
+        try:
+            module = __import__(package)
+            print(package, module.__version__)
+        except ImportError:
+            print(package, "not installed")
